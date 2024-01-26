ac88b1da3563ccf437a8c02bb5a7b2661406768d
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2024-01-26 18:49:00 -0300

    .

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,10 +1,11 @@
 var botoes = document.querySelectorAll('.botoesNum')
+var visor = document.querySelector('.visor')
 
 botoes.forEach(function(botao){
 botao.addEventListener('click', function(){
-    var visor = document.querySelectorAll(".visor")
+    
     var valorBotao = botao.value
-    visor.innerHTML = valorBotao.stringify
+    visor.innerHTML = valorBotao
     console.log(valorBotao)
 })
 
