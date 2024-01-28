04f19bc74d56eb1f3e3baeb92cc930feed36675a
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2024-01-28 12:06:35 -0300

    criei funções

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,30 +1,53 @@
 var botoesNum = document.querySelectorAll('.botoesNum')
-var botoesFunc = document.querySelectorAll('.botoesFunc')
+var botoesOpe = document.querySelectorAll('.botoesOpe')
 var visor = document.querySelector('.visor')
 var num = []
-var func = []
+var operacao = []
+var resultado = 0
 
 botoesNum.forEach(function (botao) {
     botao.addEventListener('click', function () {
-
         let valorBotao = botao.value
         num.push(valorBotao)
-        visor.innerHTML = num
+        visor.innerHTML = num.join("")
+        if(num.length > 1){
+            resul()
+            visor.innerHTML = num.join(operacao[0])+"------"+resultado
+        }
         console.log(num)
     })
 
 })
 
-botoesFunc.forEach(function (botao) {
+botoesOpe.forEach(function (botao) {
     botao.addEventListener('click', function () {
-        if (num.length === 0){
+        if (num.length === 0) {
             alert("Digite um numero")
         } else {
-        let valorBotao = botao.value
-        func.push(valorBotao)
-        visor.innerHTML = num
-        console.log(func)
+            let valorBotao = botao.value
+            operacao.push(valorBotao)
+            if(num.length > 1){
+                resul()
+            }
+            console.log(operacao)
+            visor.innerHTML = num.join(operacao[0])
         }
     })
 
 })
+
+function resul() {
+    if (operacao[0] === "x") {
+        resultado = num[0] * num[1]
+    }
+    else if (operacao[0] === "+") {
+        resultado = parseFloat(num[0]) + parseFloat(num[1])
+    }
+    else if (operacao[0] === "/") {
+        resultado = num[0] / num[1]
+    }
+    else if (operacao[0] === "-") {
+        resultado = num[0] - num[1]
+    }
+    console.log(resultado)
+}
