f5da5541dcd212613d57b88e211f4560d104adbf
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2024-01-28 19:43:35 -0300

    tentativas

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,53 +1,55 @@
 var botoesNum = document.querySelectorAll('.botoesNum')
 var botoesOpe = document.querySelectorAll('.botoesOpe')
 var visor = document.querySelector('.visor')
-var num = []
-var operacao = []
-var resultado = 0
+var numeros = []
+var operacoes = []
 
 botoesNum.forEach(function (botao) {
     botao.addEventListener('click', function () {
-        let valorBotao = botao.value
-        num.push(valorBotao)
-        visor.innerHTML = num.join("")
-        if(num.length > 1){
+        let valorBotao = parseFloat(botao.value)
+        numeros.push(valorBotao)
+        visor.innerHTML = numeros.join("")
+        if (numeros.length > 1) {
             resul()
-            visor.innerHTML = num.join(operacao[0])+"------"+resultado
         }
-        console.log(num)
+        console.log(numeros)
     })
 
 })
 
 botoesOpe.forEach(function (botao) {
     botao.addEventListener('click', function () {
-        if (num.length === 0) {
-            alert("Digite um numero")
-        } else {
-            let valorBotao = botao.value
-            operacao.push(valorBotao)
-            if(num.length > 1){
-                resul()
-            }
-            console.log(operacao)
-            visor.innerHTML = num.join(operacao[0])
-        }
-    })
+        let valorBotao = botao.value;
+        let visorConteudo = visor.innerHTML.trim();
 
-})
+        if (visorConteudo !== '') {
+            let ultimoCaractere = visorConteudo.slice(-1);
+            if (isNaN(ultimoCaractere)) {
+                visor.innerHTML = visorConteudo.slice(0, -1);
+            }
+            numeros.push(parseFloat(visorConteudo));
+            operacoes.push(valorBotao);
+            visor.innerHTML += valorBotao;
+        }
+    });
+});
+
 
 function resul() {
-    if (operacao[0] === "x") {
-        resultado = num[0] * num[1]
-    }
-    else if (operacao[0] === "+") {
-        resultado = parseFloat(num[0]) + parseFloat(num[1])
-    }
-    else if (operacao[0] === "/") {
-        resultado = num[0] / num[1]
-    }
-    else if (operacao[0] === "-") {
-        resultado = num[0] - num[1]
+    var resultado = numeros[0]
+    for (let i = 0; i < operacoes.length; i++) {
+        const operador = operacoes[i]
+        const proximoNumero = numeros[i + 1]
+
+        if (operador === '+') {
+            resultado += proximoNumero
+        } else if (operador === '-') {
+            resultado -= proximoNumero
+        } else if (operador === '/') {
+            resultado /= proximoNumero
+        } else if (operador === '*') {
+            resultado *= proximoNumero
+        }
     }
     console.log(resultado)
 }
